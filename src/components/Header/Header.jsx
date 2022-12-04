926430da46afa3ec2997d014b06cb0fb5e25fa7b
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-12-04 21:16:22 +0300

    feat: add navigation-menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 import { Navigation } from '../';
-
 
 function Header() {
   const location = useLocation().pathname;
@@ -10,13 +9,10 @@ function Header() {
     <header
       className={classNames('header', {
         header_main: location === '/',
-        header_movies: location === '/movies',
       })}>
-      <Link to="/">
-        <div className="header__logo">
-        </div>
-      </Link>
-      < Navigation className="header__navigation"/>
+      <div className="header__container">
+        <Navigation className="header__navigation" />
+      </div>
     </header>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,41 +1,120 @@
-import { Link, useLocation } from 'react-router-dom';
-// import classNames from 'classnames';
+import { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
 function Navigation() {
   const location = useLocation().pathname;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenu, setIsMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const screenWithTablet = 768;
+  const isMenuOpenClassFr = '_is-menu-open';
+  const isMenuClassFr = '_is-menu';
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    if (windowWidth <= screenWithTablet) {
+      setIsMenu(true);
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth]);
+
+  useEffect(() => {
+    if (windowWidth > screenWithTablet && (isMenu || isMenuOpen)) {
+      setIsMenu(false);
+      setIsMenuOpen(false);
+    }
+  }, [windowWidth, isMenu, isMenuOpen]);
+
+  function toggleMenuOpen() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <nav className="navigation">
+    <nav
+      className={classNames('navigation', {
+        'navigation_main': location === '/',
+        'navigation_is-menu-open': isMenuOpen,
+      })}>
       {location === '/' ? (
-        <ul className="navigation__list">
-          <li className="navigation__item-auth">
-            <Link to="/signup" className="navigation__link-auth">
-              Регистрация
-            </Link>
-          </li>
-          <li className="navigation__item-auth">
-            <Link to="/signin" className="navigation__link-auth navigation__link-auth_signin">
-              Войти
-            </Link>
-          </li>
-        </ul>
-      ) : (
         <>
-          <ul className="navigation__list">
-            <li className="navigation__item-movies">
-              <Link to="/signup" className="navigation__link-movies">
-                Фильмы
-              </Link>
+          <NavLink to="/">
+            <div className="navigation__logo"></div>
+          </NavLink>
+          <ul className="navigation__list navigation__list_auth">
+            <li className="navigation__list-item">
+              <NavLink to="/signup" className="navigation__link navigation__link_auth">
+                Регистрация
+              </NavLink>
             </li>
-            <li className="navigation__item-auth">
-              <Link to="/signin" className="navigation__link-movies">
-                Сохранённые фильмы
-              </Link>
+            <li className="navigation__list-item">
+              <NavLink
+                to="/signin"
+                className=" navigation__link navigation__link_auth navigation__link_with-bg">
+                Войти
+              </NavLink>
             </li>
           </ul>
-          <Link to="/signin" className="navigation__link-profile">
-            <p className="navigation__profile-text">Аккаунт</p>
-            <div className="navigation__profile-icon" />
-          </Link>
+        </>
+      ) : (
+        <>
+          {isMenu && (
+            <NavLink to="/">
+              <div className="navigation__logo"></div>
+            </NavLink>
+          )}
+          <div
+            onClick={toggleMenuOpen}
+            className={`
+            navigation__burger-container 
+            ${isMenuOpen ? `navigation__burger-container${isMenuOpenClassFr}` : ''}
+            ${isMenu ? `navigation__burger-container${isMenuClassFr}` : ''}
+          `}>
+            <span
+              className={`navigation__burger ${
+                isMenuOpen ? `navigation__burger${isMenuOpenClassFr}` : ''
+              }`}></span>
+          </div>
+          <div
+            className={classNames('navigation__menu', {
+              'navigation__menu_is-open': isMenuOpen,
+            })}>
+            {!isMenu && (
+              <NavLink to="/">
+                <div className="navigation__logo"></div>
+              </NavLink>
+            )}
+            <ul className="navigation__list navigation__list_movies">
+              {isMenu && (
+                <li className="navigation__list-item">
+                  <NavLink to="/" className=" navigation__link navigation__link_movies">
+                    Главная
+                  </NavLink>
+                </li>
+              )}
+              <li className="navigation__list-item">
+                <NavLink to="/movies" className=" navigation__link navigation__link_movies">
+                  Фильмы
+                </NavLink>
+              </li>
+              <li className="navigation__list-item">
+                <NavLink to="/saved-movies" className="navigation__link navigation__link_movies">
+                  Сохранённые фильмы
+                </NavLink>
+              </li>
+            </ul>
+            <NavLink to="/signin" className="navigation__link navigation__link_profile">
+              <p className="navigation__profile-text">Аккаунт</p>
+              <div className="navigation__profile-icon" />
+            </NavLink>
+          </div>
         </>
       )}
     </nav>
