44a8f2dcf941a13a8e48e5cbcf1b734cb85cf100
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-12-05 22:23:21 +0300

    feat: add Main page for desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className="footer__bottom">
+          <p className="footer__copyright">© {new Date().getFullYear()}</p>
+          <ul className="footer__links">
+            <li className="footer__item">
+              <a href="https://practicum.yandex.ru/" className="footer__link">
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="https://github.com/EugeneCod" className="footer__link">
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <div className="portfolio">
+      <div className="portfolio__container">
+        <h2 className="portfolio__title">Портфолио</h2>
+        <ul className="portfolio__list">
+          <li className="portfolio__item">
+            <a href="https://eugenecod.github.io/how-to-learn" className="portfolio__link">
+              Статичный сайт
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a href="https://eugenecod.github.io/russian-travel" className="portfolio__link">
+              Адаптивный сайт
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a href="https://mesto.ekg.nomoredomains.icu/" className="portfolio__link">
+              Одностраничное приложение
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
